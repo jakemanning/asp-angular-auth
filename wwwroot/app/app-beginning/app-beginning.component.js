44fe58e98bdf8cf096c8c62487058d34a1c8edab
Jake Manning <JManning@ethosgroup.com>
2016-07-28 15:31:29 -0500

    Updated sample project

---
+++ wwwroot/app/app-beginning/app-beginning.component.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('appBeginning', [])
+        .component('appBeginning', {
+            templateUrl: '/app/app-beginning/app-beginning.html',
+            controller: 'appBeginningController'
+        });
+})();
---
+++ wwwroot/app/app-beginning/app-beginning.controller.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+
+    angular
+      .module('appBeginning')
+      .controller('appBeginningController', appBeginningController);
+
+    appBeginningController.$inject = ['$log'];
+    function appBeginningController($log) {
+        var vm = this;
+
+        vm.title = 'App Beginning';
+
+        activate();
+
+        function activate() {
+            $log.log("App Beginning");
+        }
+    }
+})();
